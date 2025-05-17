c008528d4be30db21b6bc5f2ce8adbfe1504da7f
Imad Asserne <imad.asserne@gmail.com>
2025-05-17 01:14:19 +0000

    change cv

--- main.js
+++ main.js
@@ -95,3 +95,33 @@ scrollToTopBtn.addEventListener("click", function () {
         behavior: "smooth"
     });
 });
+
+
+
+// Menu 
+const mobileMenuBtn = document.querySelector('.mobile-menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+
+    // Change icon based on menu state
+    const icon = mobileMenuBtn.querySelector('i');
+    if (navLinks.classList.contains('active')) {
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    } else {
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
+});
+
+// Close mobile menu when a link is clicked
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        const icon = mobileMenuBtn.querySelector('i');
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    });
+});
